e986434a6c30b9f88267324ac063a4744c6fdd96
nrmerlis <nicoxeneize.10@gmail.com>
2017-05-21 18:48:55 -0300

    Chkp3

--- Prueba3.c
+++ Prueba3.c
@@ -10,6 +10,12 @@
 #define TRUE 1
 #define FALSE 0
 
+//Estos define que defino ahora luego se obtienen del config file
+#define MARCOS 4
+#define MARCO_SIZE 512
+
+
+
 
 Lista listaMemoriaLibre = NULL;
 pNodo p;
@@ -17,48 +23,56 @@ pNodo p;
 Lista listaMemoriaOcupada = NULL;
 pNodo q;
 
+void * memoria; //MARCOS * MARCOS_SIZE
+
+
+
 struct heapMetadata{
     uint32_t size;
 	bool isFree;
 };
 
-struct heapMetadataID{
-    uint32_t address;   //Direccion de memoria del HeapMetaData
-    uint32_t PID;       //PID del programa que luego del HeapMetadata
-    uint32_t PSize;     //Tamanio total del programa
-};
 //Creo esta estructura para guardar los datos con su tamanio por que si no no se puede saber con un sizeof por ser void
 struct datosStruct{
     void * dato;
     unsigned int tamDatos;
     int pid;
+
 };
 
-struct heapMetadatasContainer{
-    unsigned long int cantidad;
-    void * heapMetadatas;
-};
+struct estructuraPaginacionInversa{
+    int ** matriz;
+    int filas;
+    int columnas;
 
+};
+typedef struct estructuraPaginacionInversa tEstructuraPaginacionInversa;
+
+tEstructuraPaginacionInversa EPI;
 
 typedef struct heapMetadata metadata;
 
-typedef struct heapMetadataID metadataID;
-
 typedef struct datosStruct tDato;
+
+
 
 void * nuevoBloqueDeMemoria();
 metadata obtengoHeapMetadata(void * pagina,int posicionDeArranque);
 void dividoMemoria(void * memoria);
-bool puedoAlojarDatosEnUnaPagina(metadata a, int tamDatos);
-void * agregarDatosABloqueDeMemoria(void * bloque, tDato datos);
+bool puedoAlojarDatosEnUnaPagina(metadata memoria, int tamDatos);
+void * agregarDatosABloqueDeMemoria(void * memoria, tDato datos);
 tDato creoDato(void * dato,unsigned int tamDatos);
-bool puedoAlojarDatos(void * bloque, int tamDatos);
+bool puedoAlojarDatos(void * memoria, int tamDatos);
+bool consola();
+tDato obtenerMemoria(void * memoria,int PID);
 
 
 int main(void)
 {
 
-    void * a = nuevoBloqueDeMemoria();
+    memoria = nuevoBloqueDeMemoria();
+
+
 
     pNodo n;
 
@@ -78,7 +92,7 @@ int main(void)
 n= obtenerPrimerNodo(listaMemoriaLibre);
 metadata metaData;
 
-    metaData = obtengoHeapMetadata(a,n->posMemHeap);
+    metaData = obtengoHeapMetadata(memoria,n->posMemHeap);
     printf("Memoria libre en primer nodo libre: %d bytes \n\n",metaData.size);
 
     printf("memoria libre antes del copiado ");
@@ -90,13 +104,13 @@ metadata metaData;
 
 
 
-    a = agregarDatosABloqueDeMemoria(a,datito);
-    a = agregarDatosABloqueDeMemoria(a,dato);
+    memoria = agregarDatosABloqueDeMemoria(memoria,datito);
+    memoria = agregarDatosABloqueDeMemoria(memoria,dato);
 n= obtenerPrimerNodo(listaMemoriaLibre);
-      metaData = obtengoHeapMetadata(a,n->posMemHeap);
+      metaData = obtengoHeapMetadata(memoria,n->posMemHeap);
     printf("Memoria libre en primer nodo libre luego de un copiado: %d bytes \n\n",metaData.size);
     n= obtenerPrimerNodo(listaMemoriaOcupada->siguiente);
-      metaData = obtengoHeapMetadata(a,n->posMemHeap);
+      metaData = obtengoHeapMetadata(memoria,n->posMemHeap);
 printf("memoria ocupada del primer nodo despue del primer copiado: %d \n",metaData.size);
 
 
@@ -114,38 +128,50 @@ printf("memoria ocupada del primer nodo despue del primer copiado: %d \n",metaDa
     MostrarLista(listaMemoriaLibre,ASCENDENTE);
 
     pNodo m= obtenerPrimerNodo(listaMemoriaLibre);
-    metaData = obtengoHeapMetadata(a,m->posMemHeap);
+    metaData = obtengoHeapMetadata(memoria,m->posMemHeap);
     printf("El primer nodo libre luego de la copia es: %d \n",m->posMemHeap);
 
  printf("\nMemoria libre en primer nodo libre despues de un copiado: %d bytes \n\n",metaData.size);
 
     m= obtenerPrimerNodo(listaMemoriaOcupada);
-    metaData = obtengoHeapMetadata(a,m->siguiente->siguiente->posMemHeap);
+    metaData = obtengoHeapMetadata(memoria,m->siguiente->siguiente->posMemHeap);
     printf("\nMemoria Ocupada en tercer nodo ocupado despues de un copiado: %d bytes  \n",metaData.size);
     printf("ACLARACION: El primer nodo ocupado tiene 496 bytes, el segundo 0 y el tercero 2 bytes \n");
     printf("Pertenece al programa con PID: %d bytes  \n\n",m->siguiente->siguiente->pid);
 
     m= obtenerPrimerNodo(listaMemoriaOcupada);
-    metaData = obtengoHeapMetadata(a,m->siguiente->siguiente->siguiente->posMemHeap);
+    metaData = obtengoHeapMetadata(memoria,m->siguiente->siguiente->siguiente->posMemHeap);
     printf("\nMemoria Ocupada en cuarto nodo ocupado despues de un segundo copiado: %d bytes  \n",metaData.size);
     printf("ACLARACION: El primer nodo ocupado tiene 496 bytes, el segundo 0, el tercero 2 bytes y el cuarto 10 \n");
     printf("Pertenece al programa con PID: %d bytes  \n\n",m->siguiente->siguiente->siguiente->pid);
 
+    int temp=0;
+    for (temp;temp<EPI.filas;temp++){
+     printf("EPI nFrame PID nPagina PosMem: %d,%d,%d,%d \n\n",EPI.matriz[temp][0],EPI.matriz[temp][1],EPI.matriz[temp][2],EPI.matriz[temp][3]);
+    }
+     //,EPI.matriz[0][1],EPI.matriz[0][2]
+
+     dato=obtenerMemoria(memoria,15);
+     printf("%d \n",dato.tamDatos);
+    consola();
+
 }
 
 
 
 
-void * nuevoBloqueDeMemoria()
+void * nuevoBloqueDeMemoria()//Inicializo memora
 {
-	void * memoria = malloc(TAM_BLOQUE);
+	void * memoria = malloc(MARCOS*MARCO_SIZE);
 
     dividoMemoria(memoria); //Divido el bloque en paginas de 512 con indicadores de inicio y fin de pagina
+
+    inicializoEPI();
 
 	return memoria;
 }
 
-void * agregarDatosABloqueDeMemoria(void * bloque, tDato d)
+void * agregarDatosABloqueDeMemoria(void * memoria, tDato d)
 {
 	/*Verifico condiciones:: 1 - Existe bloque que puede alojar todo
 
@@ -172,18 +198,19 @@ void * agregarDatosABloqueDeMemoria(void * bloque, tDato d)
 
     int yaCopie=FALSE;
     int yaBorre=FALSE;
+
     tamDatos = d.tamDatos;
     datos = d.dato;
     while (nodo && tamDatos>0)//avanzo segun la lista de memoria disponible
     {
 
         nodo = obtenerPrimerNodo(listaMemoriaLibre);
-        metaData = obtengoHeapMetadata(bloque,nodo->posMemHeap);
+        metaData = obtengoHeapMetadata(memoria,nodo->posMemHeap);
         tamMetaAntesModificacion = metaData.size;
         posMem=nodo->posMemHeap;
 
 
-        if (puedoAlojarDatos(bloque,tamDatos) && tamDatos>0)
+        if (puedoAlojarDatos(memoria,tamDatos) && tamDatos>0)
         {
 
             if(tamDatos<=metaData.size) cuantoPuedoCopiar =tamDatos;
@@ -195,11 +222,20 @@ void * agregarDatosABloqueDeMemoria(void * bloque, tDato d)
 
             memcpy((nodo->posMemHeap)+sizeof(metaData),datos,cuantoPuedoCopiar); //Copio los datos que me llegan
 
+          int  nPagina=(((nodo->posMemHeap)+sizeof(metaData)- memoria)/512)-1;
+          if (nPagina==-1) nPagina+=1;
 
+
+            agregaDatoEPI(d.pid,nPagina,nodo);
 
              //Actualizo el metadata de memoria a ocupado y los bytes ocuados
             Insertar(&listaMemoriaOcupada,nodo->posMemHeap,d.pid);
+
+
+
             Borrar(&listaMemoriaLibre,nodo->posMemHeap);
+
+
 
 
 
@@ -237,10 +273,36 @@ void * agregarDatosABloqueDeMemoria(void * bloque, tDato d)
 
     }
 
-return bloque;
+return memoria;
 
     }
 
+tDato obtenerMemoria(void * memoria,int PID){
+    int i=0;
+    void *programa;
+    metadata heap;
+    int tamPrograma=0;
+    int tamProgramaAnterior=0;
+    tDato dato;
+    for(i;i<EPI.filas;i++){
+        if (EPI.matriz[i][1]==PID){
+
+            heap = obtengoHeapMetadata(memoria,EPI.matriz[i][3]);
+
+            tamProgramaAnterior=tamPrograma;
+            tamPrograma+=heap.size;
+            programa = calloc(programa,tamPrograma*sizeof(void*));
+            if(programa!='\0'){
+           memmove((void *)(programa+tamProgramaAnterior),(void *)(EPI.matriz[i][3]+sizeof(heap)),heap.size);
+
+            }
+        }
+    }
+    dato.dato=programa; // aca hay que hacer un memcpy
+    dato.tamDatos=tamPrograma;
+
+    return dato;
+}
 
 
 metadata obtengoHeapMetadata(void * pagina,int posicionDeArranque){
@@ -253,42 +315,43 @@ metadata obtengoHeapMetadata(void * pagina,int posicionDeArranque){
     return b;
 }
 
+
 void dividoMemoria(void * memoria){
 
     int i=0;
 
-    while (i<(TAM_BLOQUE/TAM_PAGINA)){
+    while (i<MARCOS){
 
 	metadata inicio;
-	inicio.size=496;
+	inicio.size=MARCO_SIZE-(sizeof(metadata)*2);
 	inicio.isFree=TRUE;
 
-	memcpy(memoria + (i*TAM_PAGINA),&inicio,sizeof(metadata));
-    Insertar(&listaMemoriaLibre,((memoria + (i*TAM_PAGINA))),0);//Agrega a listaDeMemoriaLibrea posiciones de los heaps con memoria libre
+	memcpy(memoria + (i*MARCO_SIZE),&inicio,sizeof(metadata));
+    Insertar(&listaMemoriaLibre,((memoria + (i*MARCO_SIZE))),0);//Agrega a listaDeMemoriaLibrea posiciones de los heaps con memoria libre
 
 	metadata fin;
     fin.size=0;
     fin.isFree=TRUE;
-    memcpy(memoria+(((i+1)*TAM_PAGINA)-sizeof(metadata)),&fin,sizeof(metadata));
-    Insertar(&listaMemoriaOcupada,memoria+((i+1)*TAM_PAGINA)-sizeof(metadata),0);
+    memcpy(memoria+(((i+1)*MARCO_SIZE)-sizeof(metadata)),&fin,sizeof(metadata));
+    Insertar(&listaMemoriaOcupada,memoria+((i+1)*MARCO_SIZE)-sizeof(metadata),0);
     i++;
     }
 
 }
 
-bool puedoAlojarDatosEnUnaPagina(metadata a, int tamDatos){
+bool puedoAlojarDatosEnUnaPagina(metadata memoria, int tamDatos){
 
-    return a.isFree && a.size>=tamDatos;
+    return memoria.isFree && memoria.size>=tamDatos;
 
 }
 
-bool puedoAlojarDatos(void * bloque, int tamDatos){
+bool puedoAlojarDatos(void * memoria, int tamDatos){
 
     pNodo n = obtenerPrimerNodo(listaMemoriaLibre);
     int sumador=0;
     metadata meta;
     while (n){
-        meta = obtengoHeapMetadata(bloque,n->posMemHeap);
+        meta = obtengoHeapMetadata(memoria,n->posMemHeap);
         sumador+=meta.size;
         n=n->siguiente;
     }
@@ -296,6 +359,32 @@ bool puedoAlojarDatos(void * bloque, int tamDatos){
 
     return FALSE;
 }
+void inicializoEPI(){
+
+EPI.filas=0;
+EPI.columnas=3;
+
+
+}
+void agregaDatoEPI(int PID,int nPagina,pNodo nodo){
+
+    int *aux;
+    aux = (int**)realloc(EPI.matriz,sizeof(int)*(EPI.filas+1));
+
+    if (aux!='\0'){
+    //memcpy(EPI.matriz,&aux,sizeof(int)*(EPI.filas+1));
+    aux[EPI.filas] =(int**) malloc(sizeof(int)*4);
+    EPI.matriz=aux;
+    memcpy(&EPI.matriz[EPI.filas][0],&EPI.filas,sizeof(EPI.filas));
+
+    memcpy(&EPI.matriz[EPI.filas][1],&PID,sizeof(PID));
+    memcpy(&EPI.matriz[EPI.filas][2],&nPagina,sizeof(nPagina));
+    memcpy(&EPI.matriz[EPI.filas][3],&nodo->posMemHeap,sizeof(nodo->posMemHeap));
+
+    EPI.filas+=1;
+    }
+}
+
 
 tDato creoDato(void * dato,unsigned int tamDatos){
     tDato d;
@@ -304,5 +393,39 @@ tDato creoDato(void * dato,unsigned int tamDatos){
     d.tamDatos=tamDatos;
 
     return d;
+}
+
+void dump(){
+    BorrarLista(&listaMemoriaLibre);
+    BorrarLista(&listaMemoriaOcupada);
+    free(memoria);
+}
+
+bool consola(){
+int salir = 0;
+while (!salir) {
+		//Mensaje
+		char* mensaje = (char *) malloc(sizeof(char) * 1000);
+		printf("> ");
+		scanf("%[^\n]s", mensaje);
+		char* comando = strtok(mensaje, " ");
+		char* argumento = strtok(NULL, " ");
+
+		 if (!strcmp(comando, "exit"))
+			salir = 1;
+		else if (!strcmp(comando, "clean"))
+			system("clear");
+        else if (!strcmp(comando, "dump")){
+             dump();
+			}
+		else
+			printf("Comando incorrecto. Pruebe run | stop | exit | clean\n");
+		free(mensaje);
+		limpiarBufferDeEntrada();
+	}
+}
+void limpiarBufferDeEntrada() {
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF) { }
 }
 
